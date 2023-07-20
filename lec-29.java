399855c4098bd85b483dc537c6aa1f2a2011c944
12104739 <103727187+12104739@users.noreply.github.com>
2023-07-20 17:57:14 +0530

    Update lec-29.java

--- lec-29.java
+++ lec-29.java
@@ -1,28 +1,72 @@
-package Strings;
+import java.util.*;
+public class CheckPalindrome{
+static boolean checkPalindrome(String s)
+{
+s= s.toLowerCase();
+int l = 0;
+int r = s.length() -1;
+while (l<r)
+{
+if(s.charAt(l) = s.charAt(r))
+{
+return false;
+}
+l++;
+r--;
+}
+return true;
+}
+public static void main(String[] args)
+{
+String s= "MADAVI";
+String s2= "CIPHER";
+String s3 = "Madam";
 
-import java.util.Arrays;
+System.out.println(checkPalindrome(s));
+System.out.println(checkPalindrome(s2));
+System.out.println(checkPalindrome(s3));
 
-public class StringStudy2 {
-	public static void main(String[] args) {
+}
+}
 
-	    String str = "NArasimha";
-	    System.out.println(str.startsWith("NA"));
-	    //---------------------------------------------
-	    String str1 = "narasimHA";
-	    System.out.println(str1.endsWith("HA"));
-	    //--------------------------------------------
-	    String str2 = "Java";
-	    byte[] byteArray;
-	    byteArray = str2.getBytes();
-	    System.out.println(Arrays.toString(byteArray));
-	    //--------------------------------------------
-	    String str3 = "I";
-	    String str4 = "love";
-	    String str5 = "INDIA";
-	    String joinedStr = String.join(" ", str3, str4, str5);
+=====================
+import java.util.*;
+static boolean isAnagram(String s1, String s2)
+{
+if(s1.length() ! = s2.length()) return false;
 
-	    System.out.println(joinedStr);
+s1 = s1.toLowerCase();
+s2 = s2.toLowerCase();
 
-	  }
+int n= s1.length();
 
+int freqArr1[] = new int[26];
+int freqArr2[] = new int[26];
+
+for(int i=0; i<n; i++)
+{
+int index1 = s1.charAt(i) - 'a';
+freqArr1[index1]++;
+
+int index2 = s2.charAt(i) - 'a';
+freqArr1[index2]++;
+
+for(int i =0; i<26; i++)
+{
+if(freqArr[i]!=freqArr2[i])
+{
+return false;
+}
+
+}
+return true;
+}
+
+public static void main(String[] args)
+{
+System.out.println(isAnagram("CAT", "TAC"));
+System.out.println(isAnagram("CAT", "TAG"));
+System.out.println(isAnagram("CAT", "TACC"));
+
+}
 }
